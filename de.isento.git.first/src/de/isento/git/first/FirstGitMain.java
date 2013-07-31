1563b0fb416534cd5babd0553986f35087649d68
bigtor <victor.grande.cantero@gmail.com>
2013-07-31 21:40:13 +0200

    Zweite Änderungen gecheckt! :D

--- de.isento.git.first/src/de/isento/git/first/FirstGitMain.java
+++ de.isento.git.first/src/de/isento/git/first/FirstGitMain.java
@@ -1,13 +1,16 @@
 package de.isento.git.first;
 
+import de.isento.git.first.utils.HelloWorldUtils;
+
 /**
  * Hauptklasse - Git Erste
- *
  */
 public class FirstGitMain {
 
 	public static void main(String[] args){
-		System.out.println("Hallo Isento!!");
+		
+		HelloWorldUtils helloWorld = new HelloWorldUtils();
+		
+		System.out.println(helloWorld.sayHello("Isento"));
 	}
-	
 }
---
+++ de.isento.git.first/src/de/isento/git/first/utils/HelloWorldUtils.java
@@ -0,0 +1,19 @@
+package de.isento.git.first.utils;
+
+/**
+ * Nutzbarkeit klasse
+ */
+public class HelloWorldUtils {
+
+	/**
+	 * Ohne Parameter Bauer
+	 */
+	public HelloWorldUtils(){
+		
+	}
+	
+	public String sayHello(String benutzer) {
+		return "Hallo " + benutzer + "!!";
+	}
+	
+}
