ab4a9614b1cb9cc41ab2d63a3d1ca0ef2775169b
bigtor <victor.grande.cantero@gmail.com>
2013-07-31 21:17:19 +0200

    Neue Repository erstellengit status

---
+++ de.isento.git.first/src/de/isento/git/first/FirstGitMain.java
@@ -0,0 +1,13 @@
+package de.isento.git.first;
+
+/**
+ * Hauptklasse - Git Erste
+ *
+ */
+public class FirstGitMain {
+
+	public static void main(String[] args){
+		System.out.println("Hallo Isento!!");
+	}
+	
+}
