162ccd122585aacdefa39cb5e7818f0b66cefd63
bigtor <victor.grande.cantero@gmail.com>
2013-08-01 14:13:28 +0200

    Datum und Land Meldung in Hallo Methode hinzugefügt

--- de.isento.git.first/src/de/isento/git/first/utils/BasicUtils.java
+++ de.isento.git.first/src/de/isento/git/first/utils/BasicUtils.java
@@ -1,5 +1,8 @@
 package de.isento.git.first.utils;
 
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Nutzbarkeit klasse
  */
@@ -12,8 +15,29 @@ public class BasicUtils {
 		
 	}
 	
+	/**
+	 * Erstellt ein Hallo Welt String
+	 */
 	public String sayHello(String benutzer) {
-		return "Hallo " + benutzer + "!!";
+		return sayHello(benutzer, true);
 	}
 	
+	/**
+	 * Erstellt ein Hallo Welt String
+	 */
+	public String sayHello(String benutzer, boolean showDate) {
+		String salute = "Hallo " + benutzer + "!!";
+		if (showDate) {
+			salute+=getCountryAndDate();
+		}
+		return salute;
+	}
+	
+	/**
+	 * Erstellt ein String mit Datum und Land (Locale) für diese Maschine 
+	 */
+	public String getCountryAndDate(){
+		Date date = new Date();
+		return "Datum [" + date.toString() + "], Land [" + Locale.getDefault().getCountry() + "]";
+	}
 }
